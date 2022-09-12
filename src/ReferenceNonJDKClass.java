ecc0bb96621d16f09aac3c385bba077ca2eab208
linmoug <linmoug@163.com>
2022-09-12 12:00:29 +0800

    黑马程序员全套java教程 P15

--- src/ReferenceNonJDKClass.java
+++
@@ -1,9 +0,0 @@
-import org.apache.commons.lang3.RandomUtils;
-
-public class ReferenceNonJDKClass {
-
-    public static void main(String[] args) {
-        System.out.println(RandomUtils.nextInt());
-    }
-
-}
